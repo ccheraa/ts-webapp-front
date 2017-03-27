2659946d60f01c43584598effffb636517f4a614
Ashraf Cheraa <ccheraa@gmail.com>
2017-03-27 05:45:11 +0100

    separate template file

--- src/component/app/app.component.ts
+++ src/component/app/app.component.ts
@@ -1,85 +1,14 @@
 import { Component, OnInit, Inject, HostListener, ViewChild } from '@angular/core';
 import { DOCUMENT } from '@angular/platform-browser';
 
+import { join } from 'path';
+
 import { MenuItemClass } from '@ts-webapp/common';
 import { MenuService, LoaderService, UserService, DialogService, NavigatorService } from '../../service';
 
 @Component({
   selector: 'app-root',
-  template: `
-<div class="app">
-  <div class="sidebar main" always [class.wide]="mainPane">
-    <div class="sidebar-overlay" (click)="mainPane=false"></div>
-    <span class="title">TS-APP</span>
-    <button md-icon-button menu (click)="mainPane = !mainPane" [mdTooltip]="mainPane ? 'collapse' : 'expand'" mdTooltipPosition="after">
-      <md-icon>menu</md-icon>
-      <md-icon wide>chevron_{{rtl ? 'right' : 'left'}}</md-icon>
-    </button>
-
-    <button md-icon-button *ngFor="let item of menu" (click)="goto(item.id)" [mdTooltip]="item.text" mdTooltipPosition="after"><md-icon>{{item.icon}}</md-icon></button>
-    <button md-button *ngFor="let item of menu" (click)="goto(item.id)">
-      <md-icon>{{item.icon}}</md-icon>
-      <span> {{item.text}}</span>
-    </button>
-
-    <div class="fill"></div>
-
-    <button md-icon-button mdTooltip="Settings" mdTooltipPosition="after"><md-icon>settings</md-icon></button>
-    <button md-button>
-      <md-icon>settings</md-icon>
-      <span> Settings</span>
-    </button>
-    
-    <button md-icon-button mdTooltip="About" mdTooltipPosition="after"><md-icon>info</md-icon></button>
-    <button md-button>
-      <md-icon>info</md-icon>
-      <span> About</span>
-    </button>
-
-  </div>
-  <div class="body">
-    <md-toolbar [class.shaded]="shaded">
-      <button md-icon-button back mdTooltip="Home" [disabled]="isHome ? true : null" (click)="back()"><md-icon *ngIf="!isHome">arrow_{{rtl ? 'forward' : 'back'}}</md-icon></button>
-      <span> {{title}}</span>
-      <span fl></span>
-      <button md-icon-button (click)="test()">
-        <md-icon>code</md-icon>
-      </button>
-      <button md-icon-button (click)="mainPane=!mainPane">
-        <md-icon>menu</md-icon>
-      </button>
-      <button md-icon-button (click)="sidePane=!sidePane">
-        <md-icon>chrome_reader_mode</md-icon>
-      </button>
-      <button md-icon-button (click)="dark=!dark;setDark()">
-        <md-icon>color_lens</md-icon>
-      </button>
-      <button md-icon-button (click)="rtl=!rtl;setRtl()">
-        <md-icon>swap_horiz</md-icon>
-      </button>
-      <span fl></span>
-      <button md-icon-button avatar mdTooltip="About" (click)="sidePane=!sidePane">
-        <img *ngIf="user" [src]="'/res/img/user/' + user.username + '.jpg'" alt="">
-        <md-icon *ngIf="!user">account_circle</md-icon>
-      </button>
-      <button md-icon-button mdTooltip="More..." *ngIf="vertMenu.length > 0" [mdMenuTriggerFor]="moreMenu"><md-icon>more_vert</md-icon></button>
-      <md-menu #moreMenu="mdMenu">
-        <button md-menu-item *ngFor="let item of vertMenu" (click)="item.action && item.action()">
-          <md-icon *ngIf="!!item.icon">{{item.icon}}</md-icon>
-          <span> {{item.text}}</span>
-        </button>
-      </md-menu>
-      <md-progress-bar color="primary" [hidden]="!loading" mode="query"></md-progress-bar>
-    </md-toolbar>
-    <div #body class="body-content">
-      <router-outlet></router-outlet><br>
-    </div>
-  </div>
-  <div class="sidebar side" [class.wide]="sidePane">
-    <div class="sidebar-overlay" (click)="sidePane=false"></div>
-    <app-side></app-side>
-  </div>
-</div>`,
+  templateUrl: join(__dirname, 'app.component.html'),
   styles: []
 })
 export class AppComponent implements OnInit {
